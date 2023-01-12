22dfbd35dc0b325ea301e8aec113aad381ddb542
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-12 23:58:06 +0700

    melakukan beberpa perubahan

--- server-semina-bootcamp-2/app/api/v1/categories/controller.js
+++ server-semina-bootcamp-2/app/api/v1/categories/controller.js
@@ -16,7 +16,7 @@ const create = async (req, res, next) => {
 
 const index = async (req, res, next) => {
   try {
-    const result = await Categories.find();
+    const result = await Categories.find().select('_id name');
     res.status(200).json({
       data: result,
     });
