85ea922842c8dec2c2a0cd7f70b2f81202ef9b73
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-15 12:12:36 +0700

    menambahkan controller images

--- server-semina-bootcamp-2/app/services/mongoose/images.js
+++ server-semina-bootcamp-2/app/services/mongoose/images.js
@@ -1,11 +1,22 @@
-const mongoose = require('mongoose');
-const { model, Schema } = mongoose;
+const Images = require('../../api/v1/images/model');
 
-let imageSchema = Schema(
-  {
-    name: { type: String },
-  },
-  { timestamps: true }
-);
+// 1. kita gunakan cara init
+// 2. generate url setalah submit baru kita simpan
 
-module.exports = model('image', imageSchema);
+//2. generate url setelah submit baru kita simpen images
+
+const generatedUrlImage = async (req) => {
+  const result = `uploads/${req.file.filename}`;
+
+  return result;
+};
+
+const createImages = async (req) => {
+  const result = await Images.create({
+    name: req.file ? `uploads/${req.file.filename}` : 'uploads/avatar/default.jpeg',
+  });
+
+  return result;
+};
+
+module.exports = { createImages };
