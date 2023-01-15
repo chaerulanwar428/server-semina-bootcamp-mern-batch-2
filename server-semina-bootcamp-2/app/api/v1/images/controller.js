dbde9dc1afd8a724c96715408785e2f255b8a64b
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-15 18:18:20 +0700

    logic upload avatar

--- server-semina-bootcamp-2/app/api/v1/images/controller.js
+++ server-semina-bootcamp-2/app/api/v1/images/controller.js
@@ -4,6 +4,8 @@ const { createImages } = require('../../../services/mongoose/images');
 
 const create = async (req, res) => {
   try {
+    console.log('req.file');
+    console.log(req.file);
     const result = await createImages(req);
 
     res.status(StatusCodes.CREATED).json({
