60a6bc6fe1717499521a7665d2664382bb6e5460
chaerul anwar <chaerulanwar428@gmail.com>
2023-01-23 22:25:54 +0700

    menmabahkan beberapa akses untuk owner dan organizer

--- server-semina-bootcamp-2/app/api/v1/organizers/router.js
+++ server-semina-bootcamp-2/app/api/v1/organizers/router.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express();
 const { createCMSOrganizer, createCMSUser } = require('./controller');
 
-const { authenticateUser } = require('../../../middlewares/auth');
+const { authenticateUser,authorizeRoles } = require('../../../middlewares/auth');
 
-router.post('/organizers', createCMSOrganizer);
-router.post('/users', authenticateUser,createCMSUser);
+router.post('/organizers', authenticateUser, authorizeRoles('owner'), createCMSOrganizer);
+router.post('/users',  authenticateUser, authorizeRoles('organizer'),createCMSUser);
 
 module.exports = router;
--- server-semina-bootcamp-2/app/services/mongoose/events.js
+++ server-semina-bootcamp-2/app/services/mongoose/events.js
@@ -9,7 +9,9 @@ const { NotFoundError, BadRequestError } = require('../../errors');
 
 const getAllEvents = async (req) => {
   const { keyword, category, talent } = req.query;
-  let condition = {};
+  let condition = {
+    organizer: req.user.organizer,
+  };
 
   if (keyword) {
     condition = { ...condition, title: { $regex: keyword, $options: 'i' } };
@@ -50,7 +52,7 @@ const createEvents = async (req) => {
   const check = await Events.findOne({ title });
 
   // apa bila check true / data Events sudah ada maka kita tampilkan error bad request dengan message pembicara duplikat
-  if (check) throw new BadRequestError('judul acara sudah terdaftar');
+  if (check) throw new BadRequestError('judul event duplikat');
 
   const result = await Events.create({
     title,
@@ -64,6 +66,7 @@ const createEvents = async (req) => {
     image,
     category,
     talent,
+    organizer: req.user.organizer,
   });
 
   return result;
@@ -72,7 +75,10 @@ const createEvents = async (req) => {
 const getOneEvents = async (req) => {
   const { id } = req.params;
 
-  const result = await Events.findOne({ _id: id })
+  const result = await Events.findOne({
+    _id: id,
+    organizer: req.user.organizer,
+  })
     .populate({ path: 'image', select: '_id name' })
     .populate({
       path: 'category',
@@ -105,7 +111,7 @@ const updateEvents = async (req) => {
   });
 
   // apa bila check true / data Events sudah ada maka kita tampilkan error bad request dengan message pembicara duplikat
-  if (check) throw new BadRequestError('judul acara sudah terdaftar');
+  if (check) throw new BadRequestError('judul event duplikat');
 
   const result = await Events.findOneAndUpdate(
     { _id: id },
@@ -121,6 +127,7 @@ const updateEvents = async (req) => {
       image,
       category,
       talent,
+      organizer: req.user.organizer,
     },
     { new: true, runValidators: true }
   );
@@ -136,6 +143,7 @@ const deleteEvents = async (req) => {
 
   const result = await Events.findOne({
     _id: id,
+    organizer: req.user.organizer,
   });
 
   if (!result) throw new NotFoundError(`Tidak ada pembicara dengan id :  ${id}`);
@@ -145,10 +153,29 @@ const deleteEvents = async (req) => {
   return result;
 };
 
+const changeStatusEvents = async (req) => {
+  const { id } = req.params;
+  const { statusEvent } = req.body;
+  // cari event berdasarkan field id
+  const checkEvent = await Events.findOne({
+    _id: id,
+  });
+
+  // jika id result false / null maka akan menampilkan error `Tidak ada acara dengan id` yang dikirim client
+  if (!checkEvent) throw new NotFoundError(`Tidak ada acara dengan id :  ${id}`);
+
+  checkEvent.statusEvent = statusEvent;
+
+  await checkEvent.save();
+
+  return checkEvent;
+};
+
 module.exports = {
   getAllEvents,
   createEvents,
   getOneEvents,
   updateEvents,
   deleteEvents,
+  changeStatusEvents,
 };
